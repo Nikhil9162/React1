c5e82caa8a92c65e7365271ed98be70b11115904
Nikhil9162 <patilnikhil9162@gmail.com>
2022-01-05 15:42:19 +0530

    b6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 export default function App(){
-  return (
+  let name="Nikhil";
+  let phoneno = 6464373738;
+  let email= "Nikhil@121.com";
+  return(
     <div>
-      <h1>Hello world</h1>
-      <div>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi eius soluta architecto, quis error obcaecati laboriosam nam fuga odio beatae neque molestiae eveniet ex odit ratione illo a ipsa sapiente!
-      </div>
-      <div>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum autem voluptatibus praesentium illum laudantium! Ducimus minus porro saepe molestiae quas vero veritatis sint, nemo, ipsum illo tempora assumenda omnis voluptate?
-      </div>
+      <div>{name}</div>
+      <div>{phoneno}</div>
+      <div>{email}</div>
     </div>
   )
 }
