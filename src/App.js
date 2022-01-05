000b4e8e1abd3554a695084e07c985dd12e77d5f
Nikhil9162 <patilnikhil9162@gmail.com>
2022-01-05 15:37:18 +0530

    b6

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
-const App = () => {
-  return "I am the arrow function...."
+export default function App(){
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi eius soluta architecto, quis error obcaecati laboriosam nam fuga odio beatae neque molestiae eveniet ex odit ratione illo a ipsa sapiente!
+      </div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum autem voluptatibus praesentium illum laudantium! Ducimus minus porro saepe molestiae quas vero veritatis sint, nemo, ipsum illo tempora assumenda omnis voluptate?
+      </div>
+    </div>
+  )
 }
-export default App;
